809be118ec0e2cc7e5cbfd5dbf402b44da2568f7
Salgado Uralde <inaki.salgado-uralde@capgemini.com>
2021-05-06 13:05:12 +0200

    Eliminar innecesarios empleados

--- back/src/main/java/com/recrale/accesos/repositorios/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recrale.accesos.repositorios;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.recrale.accesos.entidades.Role;
-
-public interface RolRepository extends CrudRepository<Role, Integer> {
-
-}
